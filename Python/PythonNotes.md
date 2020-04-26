2285abc92abe5d88b36e9a8a86a41cf45a2e303b
Michael Miele <v-mimiel@microsoft.com>
2020-04-25 22:52:01 -0700

    Updated content and added samples

--- Python/PythonNotes.md
+++ Python/PythonNotes.md
@@ -1,1 +1,15 @@
 # Python notes
+
+
+
+
+
+## References
+
+[!div class="mx-tdBreakAll"]
+|Link <img width=200\>|Description|
+|-------------|----------|
+|[Python documentation](https://docs.python.org/3/contents.html)|Python official documentation|
+|[Python tips](https://pythontips.com/)|&check; &star; &star; &star;Tips, learning tools, books and more.|
+|[Programiz](https://www.programiz.com/)|&check; &star; Learning Python for free. |
+|[Markdown emoji](https://github.com/StylishThemes/GitHub-Dark/wiki/Emoji)||
---
+++ Python/examples/basics/calculator.py
@@ -0,0 +1,35 @@
+import re
+
+print("Basic Calculator")
+print("Enter 'quit' to exit\n")
+
+previous = 0
+run = True
+
+def performCalculation():
+    global run
+    global previous
+
+    equation = ""
+
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        print("Goodbye")
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+
+    if previous == 0:
+        previous = eval(equation)
+    else:
+        previous = eval(str(previous) + equation)
+
+
+while run:
+    performCalculation()
+
+

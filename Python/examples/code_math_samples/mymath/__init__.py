747014e889eccd218dc796b953b1e33182f11bbe
Michael Miele <v-mimiel@microsoft.com>
2020-06-06 12:46:15 -0700

    samples changedremoved old code samples

--- Python/examples/code_math_samples/mymath/__init__.py
+++
@@ -1,6 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-"""Some doc here for mymath.
-"""
--- Python/examples/code_work_samples/__init__.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) MSSC. All rights reserved.
-# ---------------------------------------------------------
-
-"""Contains basics package code."""
-
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)

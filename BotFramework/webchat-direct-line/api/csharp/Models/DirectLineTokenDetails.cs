f5be14865a53d61463992011283bd753c2a37c6d
Michael Miele <v-mimiel@microsoft.com>
2020-07-20 22:44:38 -0700

    Web Chat using Direct Line token

---
+++ BotFramework/webchat-direct-line/api/csharp/Models/DirectLineTokenDetails.cs
@@ -0,0 +1,11 @@
+namespace TokenApi.Models
+{
+    public class DirectLineTokenDetails
+    {
+        public string Token { get; set; }
+
+        public int ExpiresIn { get; set; }
+
+        public string ConversationId { get; set; }
+    }
+}
